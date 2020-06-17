aca02805fb30740c44a0d6aaf04201d8c94269f3
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-06-17 15:26:40 +0900

    従業員一覧表示のhtmlを実装

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,5 +1,40 @@
 package jp.co.sample.controller;
 
-public class EmployeeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員情報を扱うためのコントローラーです.
+ * @author ryosuke.nakanishi
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
+	
+	
+	/**
+	 * 従業員一覧を表示するためのメソッドです.
+	 * @param model
+	 * 				従業員一覧を遷移先で表示するためのリクエストスコープです
+	 * @return
+	 * 				従業員一覧表示画面へフォワードします
+	 */
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		List<Employee> employeeList = employeeService.showList();
+		
+		model.addAttribute("employeeList", employeeList);
+		
+		return "employee/list";
+	}
 }
