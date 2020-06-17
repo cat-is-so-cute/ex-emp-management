857f401cd13f9d9b4d9b8bcb4f475f311471a5f5
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-06-17 11:45:52 +0900

    toInsertを記述

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -3,6 +3,7 @@ package jp.co.sample.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
@@ -12,16 +13,27 @@ import jp.co.sample.service.AdministratorService;
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
+	@ModelAttribute
+	private InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+	
 	@Autowired
 	private AdministratorService administratorService;
 	
 	@RequestMapping("/toInsert")
-	public String insert(InsertAdministratorForm form) {
-		Administrator administrator = new Administrator();
-		BeanUtils.copyProperties(form, administrator);
-		
-		administratorService.insert(administrator);
-		
-		return "login";
+	public String toInsert() {
+		return "/administrator/insert";
 	}
+	
+	
+//	@RequestMapping("/toInsert")
+//	public String insert(InsertAdministratorForm form) {
+//		Administrator administrator = new Administrator();
+//		BeanUtils.copyProperties(form, administrator);
+//		
+//		administratorService.insert(administrator);
+//		
+//		return "login";
+//	}
 }
