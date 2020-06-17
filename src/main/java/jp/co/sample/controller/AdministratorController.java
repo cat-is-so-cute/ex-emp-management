be95b88b68ac34863059be5ac506803ac75a4898
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-06-17 12:25:08 +0900

    Controllerクラスにlogin部分実装

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,9 +30,26 @@ public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@RequestMapping("")
 	public String toLogin() {
 		return "/administrator/login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		
+		if (administrator == null) {
+			model.addAttribute("error", "メールアドレスまたはパスワードが不正です。");
+			return "";
+		}
+		
+		session.setAttribute("administratorName", administrator.getName());
+		
+		return "/employee/showList";
 	}
 	
 	@RequestMapping("/toInsert")
