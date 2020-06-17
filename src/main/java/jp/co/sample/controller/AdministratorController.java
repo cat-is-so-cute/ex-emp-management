e144c2910eb89077208e5e05bf536a44dd540ee1
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-06-17 11:35:21 +0900

    insert部分実装

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,5 +1,27 @@
 package jp.co.sample.controller;
 
-public class AdministratorController {
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+	@Autowired
+	private AdministratorService administratorService;
+	
+	@RequestMapping("/toInsert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		
+		administratorService.insert(administrator);
+		
+		return "login";
+	}
 }
