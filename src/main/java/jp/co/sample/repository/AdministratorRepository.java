ed5198caa80c4c27f29d0f7915dbe2b71a5724e0
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-06-17 10:47:07 +0900

    AdministratorRepositoryにsignUp機能を追加

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -1,5 +1,39 @@
 package jp.co.sample.repository;
 
-public class AdministratorRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import jp.co.sample.domain.Administrator;
+
+@Repository
+public class AdministratorRepository {
+	public static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAddress(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("password"));
+		
+		return administrator;
+	};
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	public Administrator signUp(Administrator administrator) {
+		String sql = "INSERT INTO administrators VALUES (:name, :mail_address, :password);";
+		
+		MapSqlParameterSource param = new MapSqlParameterSource().addValue("name", administrator.getName())
+																 .addValue("mail_address", administrator.getMailAddress())
+																 .addValue("password", administrator.getPassword());
+		
+		template.update(sql, param);
+		
+		return administrator;
+	}
+	
+	
 }
