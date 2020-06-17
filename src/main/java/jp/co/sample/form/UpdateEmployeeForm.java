f767a17f9d327e717ebf12a98aa261d7a6f5c536
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-06-17 15:11:40 +0900

    従業員操作用のサービスクラスと更新用フォームの完成

--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -1,6 +1,36 @@
 package jp.co.sample.form;
 
+
+/**
+ * 従業員情報更新時に入力を受け取るフォームです.
+ * @author ryosuke.nakanishi
+ *
+ */
 public class UpdateEmployeeForm {
+	/** 従業員ID */
 	private String id;
+	/** 扶養人数 */
 	private String dependentsCount;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDependentsCount() {
+		return dependentsCount;
+	}
+
+	public void setDependentsCount(String dependentsCount) {
+		this.dependentsCount = dependentsCount;
+	}
+
+	@Override
+	public String toString() {
+		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+	}
+
 }
